5036a272d30eca2f0668ec49a30d83a9b7968715
Rohan Woodcock <rohanwoodcock@gmail.com>
2026-08-15 17:01:04 +0100

    default axis step change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1200,7 +1200,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT {33.33, 80, 1, 400, 500}
+#define DEFAULT_AXIS_STEPS_PER_UNIT {33.33, 53.33, 1, 400, 500}
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
